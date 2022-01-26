32da173c0b48c3c2a14573c25a7698dcd7ff35d7
CI010 <cijhn@hotmail.com>
2022-01-26 15:56:02 +0800

    feat: bump dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import * as Azure from '@azure/storage-blob';
-import { stat, readdir, createReadStream } from "fs";
-import { promisify } from 'util';
+import { getInput, info, setFailed } from '@actions/core';
+import { AnonymousCredential, BlockBlobClient, BlockBlobUploadOptions, StorageSharedKeyCredential } from '@azure/storage-blob';
+import { createReadStream, readdir, stat } from "fs";
 import { join } from 'path';
-import { getInput, setFailed, info } from '@actions/core';
+import { promisify } from 'util';
 
 async function run() {
     try {
@@ -11,23 +11,18 @@ async function run() {
         const dir = getInput('directory', { required: true });
 
         const accountKey = process.env.AZURE_ACCOUNT_KEY;
-        const token = process.env.AZURE_STORAGE_TOKEN;
 
-        let credit: Azure.Credential;
+        let credit: StorageSharedKeyCredential | AnonymousCredential;
         if (typeof accountKey === 'string') {
-            credit = new Azure.SharedKeyCredential(account, accountKey);
+            credit = new StorageSharedKeyCredential(account, accountKey);
             info('Found and use SharedKeyCredential (accountKey)');
-        } else if (typeof token === 'string') {
-            credit = new Azure.TokenCredential(token);
-            info('Found and use TokenCredential');
         } else {
-            credit = new Azure.AnonymousCredential();
+            credit = new AnonymousCredential();
             info('Not found any credential. Use AnonymousCredential. If you want assign credential, please assign env variable AZURE_ACCOUNT_KEY (your storage account key) or AZURE_STORAGE_TOKEN (your storage token)');
         }
-        const pipeline = Azure.StorageURL.newPipeline(credit);
         const s = promisify(stat);
 
-        let files = await promisify(readdir)(dir);
+        const files = await promisify(readdir)(dir);
 
         await Promise.all(files.map(async (fileName) => {
             const filePath = join(dir, fileName);
@@ -37,17 +32,17 @@ async function run() {
                 return;
             }
 
-            const options: Azure.IBlockBlobUploadOptions = {
+            const options: BlockBlobUploadOptions = {
                 blobHTTPHeaders: {
                 }
             };
             if (fileName.endsWith("yml")) { // if the file is the yml file use yml format
                 options.blobHTTPHeaders!.blobContentType = 'text/x-yaml';
             }
-            const blobURL = new Azure.BlockBlobURL(`https://${account}.blob.core.windows.net/${container}/${fileName}`, pipeline);
+            const client = new BlockBlobClient(`https://${account}.blob.core.windows.net/${container}/${fileName}`, credit)
             info(`Upload ${fileName}`);
-            await blobURL.upload(Azure.Aborter.none,
-                () => createReadStream(filePath),
+
+            await client.upload(() => createReadStream(filePath),
                 fileStat.size,
                 options);
         }));
